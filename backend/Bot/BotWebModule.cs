c1bfdb40d718e75c89d19a92cc2b8889fc1b4fed
FeroxFoxxo <feroxfoxxo@gmail.com>
2022-02-19 21:14:38 +1300

    Fixed compile bug

--- backend/Bot/BotWebModule.cs
+++ backend/Bot/BotWebModule.cs
@@ -64,7 +64,6 @@ public class BotWebModule : WebModule
 				options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 				options.CorrelationCookie.SameSite = SameSiteMode.Lax;
 				options.CorrelationCookie.HttpOnly = false;
-				options.
 			});
 
 		if (settings.CorsEnabled)
