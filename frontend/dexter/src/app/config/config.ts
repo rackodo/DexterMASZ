43aa30d68da82baeaa0a0d42b6937e35d75fdb24
FeroxFoxxo <feroxfoxxo@gmail.com>
2022-02-20 15:32:31 +1300

    Undid change to config

--- frontend/dexter/src/app/config/config.ts
+++ frontend/dexter/src/app/config/config.ts
@@ -1,12 +1,12 @@
 import { LanguageSelect } from "../models/LanguageSelect";
 
-//export const APP_BASE_URL = 'https://dexter.usfurries.com';
-export const APP_BASE_URL = 'http://127.0.0.1:5565';
+export const APP_BASE_URL = 'https://dexter.usfurries.com';
+//export const APP_BASE_URL = 'http://127.0.0.1:5565';
 
 export const API_URL = APP_BASE_URL + '/api/v1';
 
-//export const ENABLE_CORS = false;
-export const ENABLE_CORS = true;
+export const ENABLE_CORS = false;
+//export const ENABLE_CORS = true;
 
 export const DEFAULT_LANGUAGE = 'en';
 export const LANGUAGES: LanguageSelect[] = [
