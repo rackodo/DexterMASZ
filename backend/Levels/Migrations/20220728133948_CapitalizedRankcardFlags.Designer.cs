31e13bd24f1c2e4568f9293d3e688e219a34a307
EnderDoom77 <37478624+EnderDoom77@users.noreply.github.com>
2022-07-28 15:41:42 +0200

    Capitalization DB Migration

--- backend/Levels/Migrations/20220728133948_CapitalizedRankcardFlags.Designer.cs
+++ backend/Levels/Migrations/20220728133948_CapitalizedRankcardFlags.Designer.cs
@@ -2,6 +2,7 @@
 using Levels.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Levels.Migrations
 {
     [DbContext(typeof(LevelsDatabase))]
-    partial class LevelsDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20220728133948_CapitalizedRankcardFlags")]
+    partial class CapitalizedRankcardFlags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Levels/Migrations/20220728133948_CapitalizedRankcardFlags.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Levels.Migrations
+{
+    public partial class CapitalizedRankcardFlags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "rankcardFlags",
+                schema: "Levels",
+                table: "UserRankcardConfigs",
+                newName: "RankcardFlags");
+
+            migrationBuilder.RenameColumn(
+                name: "Voicexp",
+                schema: "Levels",
+                table: "GuildUserLevels",
+                newName: "VoiceXp");
+
+            migrationBuilder.RenameColumn(
+                name: "Textxp",
+                schema: "Levels",
+                table: "GuildUserLevels",
+                newName: "TextXp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RankcardFlags",
+                schema: "Levels",
+                table: "UserRankcardConfigs",
+                newName: "rankcardFlags");
+
+            migrationBuilder.RenameColumn(
+                name: "VoiceXp",
+                schema: "Levels",
+                table: "GuildUserLevels",
+                newName: "Voicexp");
+
+            migrationBuilder.RenameColumn(
+                name: "TextXp",
+                schema: "Levels",
+                table: "GuildUserLevels",
+                newName: "Textxp");
+        }
+    }
+}
