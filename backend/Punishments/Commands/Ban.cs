886d3e3cd42625bdeb8cfd95d5b896be31b8b696
ferox <feroxfoxxo@gmail.com>
2022-08-31 01:06:40 +1200

    Flipped time span

--- backend/Punishments/Commands/Ban.cs
+++ backend/Punishments/Commands/Ban.cs
@@ -24,10 +24,10 @@ public class Ban : Command<Ban>
 		string title,
 		[Summary("user", "User to punish")]
 		IUser user,
-		[Summary("time", "The time to punish the user for")]
-		TimeSpan time = default,
 		[Summary("description", "The description of the mod case")]
-		string description = "")
+		string description = "",
+		[Summary("time", "The time to punish the user for")]
+		TimeSpan time = default)
 	{
 		ModCaseRepository.AsUser(Identity);
 		GuildConfigRepository.AsUser(Identity);
