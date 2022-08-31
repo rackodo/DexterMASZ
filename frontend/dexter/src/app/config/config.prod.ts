8bc0cc517c148053a1ed98474a87adf52377114d
ferox <feroxfoxxo@gmail.com>
2022-09-01 02:38:59 +1200

    Made it so you don't have to stage config anymore.

--- frontend/dexter/src/app/config/config.prod.ts
+++ frontend/dexter/src/app/config/config.prod.ts
@@ -1,7 +1,7 @@
 import { LanguageSelect } from "../models/LanguageSelect";
 
-export const APP_BASE_URL = 'http://127.0.0.1:5565';
-export const ENABLE_CORS = true;
+export const APP_BASE_URL = 'https://dexter.usfurries.com';
+export const ENABLE_CORS = false;
 
 export const API_URL = APP_BASE_URL + '/api/v1';
 
