32515746a2b4f3b788f567ff7732e65619e2051a
FeroxFoxxo <feroxfoxxo@gmail.com>
2022-11-23 02:18:58 +1300

    Set lavalink to initialize on startup.

---
+++ backend/Music/Services/MusicService.cs
@@ -0,0 +1,28 @@
+﻿using Bot.Abstractions;
+using Discord.WebSocket;
+using Lavalink4NET;
+using Lavalink4NET.Tracking;
+
+namespace Music.Services;
+
+public class MusicService : IEvent
+{
+    private readonly DiscordSocketClient _client;
+    private readonly InactivityTrackingService _inactivityTracker;
+    private readonly IAudioService _lavalink;
+
+    public MusicService(DiscordSocketClient client, IAudioService lavalink, InactivityTrackingService inactivityTracker)
+    {
+        _client = client;
+        _lavalink = lavalink;
+        _inactivityTracker = inactivityTracker;
+    }
+
+    public void RegisterEvents() => _client.Ready += SetupLavalink;
+
+    private async Task SetupLavalink()
+    {
+        await _lavalink.InitializeAsync();
+        if (!_inactivityTracker.IsTracking) _inactivityTracker.BeginTracking();
+    }
+}
