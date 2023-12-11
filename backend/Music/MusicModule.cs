d0f2a33e1fc996610381bc49627f905c658747df
FeroxFoxxo <feroxfoxxo@gmail.com>
2023-12-12 05:18:29 +1300

    Use public ip instead of local host

--- backend/Music/MusicModule.cs
+++ backend/Music/MusicModule.cs
@@ -1,31 +1,30 @@
 ﻿using Bot.Abstractions;
 using Bot.Models;
 using Bot.Services;
-using Discord.WebSocket;
 using Fergun.Interactive;
 using Lavalink4NET.Artwork;
-using Lavalink4NET.Clients;
-using Lavalink4NET.DiscordNet;
 using Lavalink4NET.Extensions;
 using Lavalink4NET.InactivityTracking;
 using Lavalink4NET.InactivityTracking.Extensions;
 using Lavalink4NET.InactivityTracking.Trackers.Idle;
 using Lavalink4NET.Lyrics;
-using Lavalink4NET.Tracking;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 
 namespace Music;
 
 public class MusicModule : Module
 {
-    public const string Host = "localhost";
     public const int Port = 2333;
     public const string Pass = "youshallnotpass";
 
     public override string[] Contributors { get; } = ["Swyreee", "Ferox"];
 
     public override void AddServices(IServiceCollection services, CachedServices cachedServices,
-        AppSettings appSettings) =>
+        AppSettings appSettings)
+    {
+        var host = GetMyIp().ToString();
+
         services
             .AddSingleton(new InteractiveConfig { DefaultTimeout = TimeSpan.FromMinutes(5) })
             .AddSingleton<InteractiveService>()
@@ -47,10 +46,33 @@ public class MusicModule : Module
 
             .ConfigureLavalink(config =>
             {
-                config.BaseAddress = new Uri($"http://{Host}:{Port}");
-                config.WebSocketUri = new Uri($"ws://{Host}:{Port}/v4/websocket");
+                config.BaseAddress = new Uri($"http://{host}:{Port}");
+                config.WebSocketUri = new Uri($"ws://{host}:{Port}/v4/websocket");
                 config.ReadyTimeout = TimeSpan.FromSeconds(10);
                 config.Passphrase = Pass;
             })
             .AddLavalink();
+    }
+
+    public static IPAddress GetMyIp()
+    {
+        var services = new List<string>()
+        {
+            "https://ipv4.icanhazip.com",
+            "https://api.ipify.org",
+            "https://ipinfo.io/ip",
+            "https://checkip.amazonaws.com",
+            "https://wtfismyip.com/text",
+            "http://icanhazip.com"
+        };
+
+        using var webclient = new HttpClient();
+
+        foreach (var service in services)
+            try {
+                return IPAddress.Parse(webclient.GetStringAsync(service).Result);
+            } catch { }
+
+        return null;
+    }
 }
